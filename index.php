caab3aad9fadb55b301fdbabd2d2e4b61a039b02
johhnybutters <john.lopez1219@live.com>
2017-05-06 21:44:28 -0500

    edit5

--- index.php
+++ index.php
@@ -101,9 +101,6 @@
             <div class="row">
                 <div class="col-lg-12">
                     <h1>Users</h1>
-                    <?php
-                      echo "hello";
-                     ?>
                 </div>
                   </div>
         </div>
